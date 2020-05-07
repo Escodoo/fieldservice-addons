69f757e5214e95e887af118eb7282b5aeb38298c
Marcel Savegnago <marcel.savegnago@gmail.com>
2020-05-07 20:26:00 -0300

    Primeiro commit
    Este módulo foi adapatado do módulo https://github.com/ursais/osi-addons/tree/12.0/helpdesk_fieldservice

---
+++ fieldservice_helpdesk/__manifest__.py
@@ -0,0 +1,24 @@
+# Copyright (C) 2019 - TODAY, Open Source Integrators
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+{
+    'name': 'Helpdesk MGMT - Field Service',
+    'summary': 'Create service requests from a ticket',
+    'version': '12.0.1.1.0',
+    'license': 'LGPL-3',
+    'author': 'Open Source Integrators',
+    'category': 'Helpdesk',
+    'website': 'https://github.com/ursais/osi-addons',
+    'depends': [
+        'helpdesk_mgmt',
+        'fieldservice',
+    ],
+    'data': [
+        #'security/ir.model.access.csv',
+        'views/helpdesk_ticket_views.xml',
+        'views/fsm_location_views.xml',
+        'views/fsm_order_views.xml',
+        'views/res_partner.xml',
+        'wizard/fsm_order_close_wizard.xml',
+    ],
+    'installable': True,
+}
